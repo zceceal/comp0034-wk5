a1f03a31c8f0819958148776104373bb72117401
nicholsons <sarah.sanders@ucl.ac.uk>
2024-02-02 13:47:23 +0000

    updated models as it should be for the start of the activities

--- paralympics/models.py
+++ paralympics/models.py
@@ -49,13 +49,4 @@ class Event(db.Model):
 class User(db.Model):
     id: Mapped[int] = mapped_column(db.Integer, primary_key=True)
     email: Mapped[str] = mapped_column(db.String, unique=True, nullable=False)
-    password_hash: Mapped[str] = mapped_column(db.String, unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User {}>'.format(self.email)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    password: Mapped[str] = mapped_column(db.String, unique=True, nullable=False)
